c7d2b56a2757dd01ef998d44f16dd66844080e6f
De'Jour Ford <dejourford@my.unt.edu>
2023-04-12 17:56:10 -0500

    created function to create rows of items to cart

--- shopping-cart.js
+++ shopping-cart.js
@@ -1,7 +1,7 @@
 ls = localStorage
 const shoppingCart = document.querySelector('.shopping-cart')
 const invoiceNode = document.querySelector('.invoice')
-
+const subtotalPrice = document.querySelector('#subtotal-price')
 
 // // NAV CLOSE
 const closeBtn = document.querySelector('.fa-xmark')
@@ -23,47 +23,62 @@ openMenu = () => {
 
 
 
-
-
-
-let storedItems = ls.getItem('cart')
-let storedItemsToParse = JSON.parse(storedItems)
-for (let i = 0; i < storedItemsToParse.length; i++){
-     const createRow = () => {
-        const cartRow = document.createElement('section')
-        cartRow.className = 'cart-row'
-        
-        
-        const cartLeft = document.createElement('div')
-        const itemImage = document.createElement('img')
-        const itemTitle = document.createElement('span')
-        itemImage.src = storedItemsToParse[i].img
-        itemTitle.className = 'title'
-        itemTitle.textContent = storedItemsToParse[i].title
-        cartLeft.className = 'shopping-cart-left'
-        
-        
-        const cartRight = document.createElement('div')
-        const itemPrice = document.createElement('span')
-        itemPrice.className = 'price'
-        itemPrice.textContent = storedItemsToParse[i].price
-        cartRight.className = 'shopping-cart-right'
-        cartRow.append(cartLeft, cartRight) 
-        cartLeft.append(itemImage, itemTitle)
-        cartRight.appendChild(itemPrice)
-        
-        shoppingCart.insertBefore(cartRow, invoiceNode)
-
-
-    }
+function createRow(cartItem) {
+    const cartRow = document.createElement('section')
+    cartRow.className = 'cart-row'
     
-     
-    createRow()
+    
+    const cartLeft = document.createElement('div')
+    const itemImage = document.createElement('img')
+    const itemTitle = document.createElement('span')
+    itemImage.src = cartItem.img
+    itemTitle.className = 'title'
+    itemTitle.textContent = cartItem.title
+    cartLeft.className = 'shopping-cart-left'
+    
+    
+    const cartRight = document.createElement('div')
+    const itemPrice = document.createElement('span')
+    itemPrice.className = 'price'
+    itemPrice.textContent = cartItem.price
+    cartRight.className = 'shopping-cart-right'
+    cartRow.append(cartLeft, cartRight) 
+    cartLeft.append(itemImage, itemTitle)
+    cartRight.appendChild(itemPrice)
+    
+    return cartRow
     
 }
 
 
-//assign row elements from each object in ls array
+function addToCartTotal(cartItem) {
+    let sum = 0
+
+}
+
+
+let sum = 0
+let storedItems = ls.getItem('cart')
+let storedItemsToParse = JSON.parse(storedItems)
+for (let i = 0; i < storedItemsToParse.length; i++){
+     const row = createRow(storedItemsToParse[i])
+     shoppingCart.insertBefore(row, invoiceNode)
+    
+     
+     sum += Number(storedItemsToParse[i].price.slice(1))
+     let sumToFixed = sum.toFixed(2)
+     console.log(sumToFixed)
+    
+
+    
+}
+
+
+// get all object in local storage array
+// target prices
+// add all prices together
+// display in DOM
+
 
 
 
