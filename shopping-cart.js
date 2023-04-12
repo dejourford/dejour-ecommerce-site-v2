30db7b0aff960d60fcbc8cc33c3e70872f33f918
De'Jour Ford <dejourford@my.unt.edu>
2023-04-12 16:45:22 -0500

    created function for rows of items in cart

--- shopping-cart.js
+++ shopping-cart.js
@@ -1,5 +1,8 @@
 ls = localStorage
- 
+const shoppingCart = document.querySelector('.shopping-cart')
+const invoiceNode = document.querySelector('.invoice')
+
+
 // // NAV CLOSE
 const closeBtn = document.querySelector('.fa-xmark')
 const mobileNav =  document.querySelector('.mobile-nav')
@@ -18,10 +21,49 @@ openMenu = () => {
    
 }
 
-let storedTotals = ls.getItem('subtotal')
-const numbers = JSON.parse(storedTotals)
-const sum = numbers.reduce( (total, number) =>
-    total + number, 0).toFixed(2);
-console.log(sum)
+
+
+
+
+
+let storedItems = ls.getItem('cart')
+let storedItemsToParse = JSON.parse(storedItems)
+for (let i = 0; i < storedItemsToParse.length; i++){
+     const createRow = () => {
+        const cartRow = document.createElement('section')
+        cartRow.className = 'cart-row'
+        
+        
+        const cartLeft = document.createElement('div')
+        const itemImage = document.createElement('img')
+        const itemTitle = document.createElement('span')
+        itemImage.src = storedItemsToParse[i].img
+        itemTitle.className = 'title'
+        itemTitle.textContent = storedItemsToParse[i].title
+        cartLeft.className = 'shopping-cart-left'
+        
+        
+        const cartRight = document.createElement('div')
+        const itemPrice = document.createElement('span')
+        itemPrice.className = 'price'
+        itemPrice.textContent = storedItemsToParse[i].price
+        cartRight.className = 'shopping-cart-right'
+        cartRow.append(cartLeft, cartRight) 
+        cartLeft.append(itemImage, itemTitle)
+        cartRight.appendChild(itemPrice)
+        
+        shoppingCart.insertBefore(cartRow, invoiceNode)
+
+
+    }
+    
+     
+    createRow()
+    
+}
+
+
+//assign row elements from each object in ls array
+
 
 
